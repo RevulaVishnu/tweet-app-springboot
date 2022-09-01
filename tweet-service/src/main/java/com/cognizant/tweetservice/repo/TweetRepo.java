2cf4e00d5a994ccc909c62e050d92c5dbad52d39
vishnurevula <vishnurevula14@gmail.com>
2022-09-01 19:20:22 +0530

    Working on Tweet-Service

--- tweet-service/src/main/java/com/cognizant/tweetservice/repo/TweetRepo.java
+++ tweet-service/src/main/java/com/cognizant/tweetservice/repo/TweetRepo.java
@@ -1,11 +1,11 @@
 package com.cognizant.tweetservice.repo;
 
+import java.util.Optional;
+
 import com.cognizant.tweetservice.model.Tweet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface TweetRepo extends MongoRepository<Tweet, Long> {
