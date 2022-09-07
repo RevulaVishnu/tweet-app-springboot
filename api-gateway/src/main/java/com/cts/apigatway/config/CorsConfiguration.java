470acf72497cf514e57204b93815afa81d3b326b
vishnurevula <vishnurevula14@gmail.com>
2022-09-07 23:00:55 +0530

    Date format changed.

---
+++ api-gateway/src/main/java/com/cts/apigatway/config/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.cts.apigatway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfiguration extends
+        org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }}
