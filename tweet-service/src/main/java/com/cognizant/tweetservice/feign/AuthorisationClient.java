c07cd6fa8f42ff647a0c480c9d737cf09ec5737c
vishnurevula <vishnurevula14@gmail.com>
2022-09-13 10:09:23 +0530

    Load balancer in feign

--- tweet-service/src/main/java/com/cognizant/tweetservice/feign/AuthorisationClient.java
+++ tweet-service/src/main/java/com/cognizant/tweetservice/feign/AuthorisationClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
  * Feign client to connect with authorization microservice for token validation
  *
  */
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service", url = "tweet-LoadB-1RVB8HGCA28D3-cdb2a7c7804b5dd7.elb.us-east-1.amazonaws.com:8084")
 public interface AuthorisationClient {
 	
 	/**
